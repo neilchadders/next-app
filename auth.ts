1919df7b2efb6a44da4af50b60737fda931941de
neilchadders <neilchadders@hotmail.com>
2025-03-16 11:31:14 +0000

    add customise JWT token

--- auth.ts
+++ auth.ts
@@ -57,6 +57,7 @@ export const config = {
       
       async session({ session, user, trigger, token }: any) {
         // Set the user ID from the token
+       
         session.user.id = token.sub; // Set the user ID from the token
         session.user.role = token.role; 
         session.user.name = token.name;
@@ -68,8 +69,28 @@ export const config = {
   
         return session;
       },
+
+      async jwt({ token, user, trigger, session }: any) {
+        // Assign user fields to token
+        if (user) {
+          token.role = user.role;
+  
+          // If user has no name then use the email
+          if (user.name === 'NO_NAME') {
+            token.name = user.email!.split('@')[0];
+  
+            // Update database to reflect the token name
+            await prisma.user.update({
+              where: { id: user.id },
+              data: { name: token.name },
+            });
+          }
+        }
+        return token;
     },
+  },
 } satisfies NextAuthConfig;
 
-export const {handlers, auth, signIn, signOut} = NextAuth(config);
+
+
 
